edf3fc08c501aa19f7c37c0c44766491dec78160
EmilyDSarani <emily.sarani@gmail.com>
2022-01-12 14:56:54 -0600

    comment coded out the prevState stuff, will readdress it later

--- src/hooks/useColorPicker.js
+++ src/hooks/useColorPicker.js
@@ -1,38 +1,38 @@
 import { useEffect, useState } from 'react';
 
-export default function useColorPicker(initialState){
+export default function useColorPicker(){
   //naming and defining the useState
-  // const [fgColor, setFgColor] = useState('#ffcc00');
-  // const [bgColor, setBgColor] = useState('#212121');
-  // const [content, setContent] = useState('Hello, world!');
-  // const [didChangeColor, setDidChangeColor] = useState(false);
+  const [fgColor, setFgColor] = useState('#ffcc00');
+  const [bgColor, setBgColor] = useState('#212121');
+  const [content, setContent] = useState('Hello, world!');
+  const [didChangeColor, setDidChangeColor] = useState(false);
 
-  const [color, setColorState] = useState(initialState)
+  // const [color, setColorState] = useState(initialState)
 
   //handleChange magic got moved here
   const handleChange = (event) => {
-    setColorState((previousState) => {
-      return {...previousState, [event.target.name]: event.target.value };
-    })
-    // const { name, value } = event.target;
-    // switch (name) {
-    //   case 'fgColor':
-    //     setFgColor(value);
-    //     setDidChangeColor(true);
-    //     break;
-    //   case 'bgColor':
-    //     setBgColor(value);
-    //     setDidChangeColor(true);
-    //     break;
-    //   case 'content':
-    //     setDidChangeColor(false);
-    //     setContent(value);
-    //     break;
-    //   default:
-    //     break;
-    // }
+    // setColorState((previousState) => {
+    //   return {...previousState, [event.target.name]: event.target.value };
+    // })
+    const { name, value } = event.target;
+    switch (name) {
+      case 'fgColor':
+        setFgColor(value);
+        setDidChangeColor(true);
+        break;
+      case 'bgColor':
+        setBgColor(value);
+        setDidChangeColor(true);
+        break;
+      case 'content':
+        setDidChangeColor(false);
+        setContent(value);
+        break;
+      default:
+        break;
+    }
   };
   //return the naming convention to use later when exported
   //remember that the order will need to match in ColorPicker jsx when we deconstruct it
-return [color, handleChange]
+return [fgColor, bgColor, content, didChangeColor, handleChange]
 }
--- src/views/ColorPicker/ColorPicker.jsx
+++ src/views/ColorPicker/ColorPicker.jsx
@@ -6,23 +6,25 @@ import useColorPicker from '../../hooks/useColorPicker';
 
 export default function ColorPicker() {
   //putting affirmation first so that the useColorPicker knows what bgcolor and fgcolor is from useAffirm
-
+  const affirmation = useAffirmations(bgColor, fgColor);
 //all of our exports from useColorPicker
 //order from the hook needs to match when we destructor it
-const [color, handleChange] = useColorPicker({
-  fgColor:'#ffcc00',
-  bgColor:'#212121',
-  content:'Hello world!',
-  didChangeColor: false,
-})
-const affirmation = useAffirmations({...color}); 
+const [fgColor, bgColor, content, didChangeColor, handleChange] = useColorPicker();
+
+// const [color, handleChange] = useColorPicker({
+//   fgColor:'#ffcc00',
+//   bgColor:'#212121',
+//   content:'Hello world!',
+//   didChangeColor: false,
+// })
+// const affirmation = useAffirmations({...color}); 
 
 
   return (
     <>
       <fieldset className={styles.colorPickerForm}>
         <legend>
-          {color.didChangeColor
+          {didChangeColor
             ? affirmation
             : 'Pick some colors and a message to display!'}
         </legend>
@@ -30,25 +32,25 @@ const affirmation = useAffirmations({...color});
           type="color"
           name="fgColor"
           aria-label="foreground color"
-          value={color.fgColor}
+          value={fgColor}
           onChange={handleChange}
         />
         <input
           type="color"
           name="bgColor"
           aria-label="background color"
-          value={color.bgColor}
+          value={bgColor}
           onChange={handleChange}
         />
         <input
           type="text"
           name="content"
           aria-label="content"
-          value={color.content}
+          value={content}
           onChange={handleChange}
         />
       </fieldset>
-      <Display content={color.content} bgColor={color.bgColor} fgColor={color.fgColor} />
+      <Display content={content} bgColor={bgColor} fgColor={fgColor} />
     </>
   );
 }
